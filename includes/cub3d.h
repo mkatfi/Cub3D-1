43aa1143d6793769a7b3ff2ed4cfcdba2d6be388
Ismail Antar <iantar@e2r8p10.1337.ma>
2023-05-23 22:51:00 +0100

    iantar

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: iantar <iantar@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 11:36:26 by iantar            #+#    #+#             */
-/*   Updated: 2023/05/23 11:42:57 by iantar           ###   ########.fr       */
+/*   Updated: 2023/05/23 21:38:57 by iantar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,21 @@
 #include "math.h"
 #include "../GNL/get_next_line.h"
 #include <unistd.h>
+#include <stdio.h>
 
+typedef struct s_data
+{
+	void	*mlx;
+	void	*mlx_win;
+	char	**map;
+	void	*img;
+	char	*get_adr;
+}		t_data;
+
+typedef struct s_pos
+{
+	int	x;
+	int	y;
+}		t_pos;
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: iantar <iantar@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 20:28:35 by iantar            #+#    #+#             */
-/*   Updated: 2023/05/23 12:14:00 by iantar           ###   ########.fr       */
+/*   Updated: 2023/05/23 22:49:41 by iantar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	high_count(fd)
 	return (len);
 }
 
-// int	width_count(fd)
+// int	width_count()
 // {
 // 	char	*line;
 	
@@ -42,24 +42,119 @@ int	high_count(fd)
 	
 // }
 
-char	**get_map()
+char	**get_map(void)
 {
 	char	**map;
 	int		i;
+	int		j;
+	char	c;
 
-	map = malloc(sizeof(char *) * 10);
-	while (i < )
+	map = malloc(sizeof(char *) * 11);
+	i = 0;
+	while (i < 10)
+	{
+		map[i] = malloc(11 * sizeof(char));
+		j = 0;
+		while (j < 10)
+		{
+			if (i == 0 || i == 9 || j == 0 || j == 9 || i == j)
+				c = '1';
+			else
+				c = '0';
+			map[i][j] = c;
+			j++;
+		}
+		map[i][j] = '\0';
+		i++;		
+	}
+	map[i] = NULL;
+	return (map);
 }
 
+void	print_map(char **map)
+{
+	int	i;
+
+	i = -1;
+	while (++i < 11)
+		printf("%s\n", map[i]);
+}
+
+void	draw_square(int ref, int size, t_data data)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < size)
+	{
+		y = 0;
+		while (y < size)
+		{
+			mlx_pixel_put(data.mlx, data.mlx_win, x + ref, y + ref, 0x00FF0000);
+			y++;
+		}
+		x++;
+	}
+}
+
+void	put_square(t_pos pos, int size, t_data data)
+{
+	int	i;
+	int	bits_per_pixel;
+	int	size_line;
+	int	endian;
+
+	i = 0;
+	data.img = mlx_new_image(data.mlx, size, size);
+	data.get_adr = mlx_get_data_addr(data.img, &bits_per_pixel, &size_line, &endian);
+	while (i < size * size_line)
+	{
+		if (i % 4 == 0)
+			data.get_adr[i] = 0;//bleu
+		if (i % 4 == 1)
+			data.get_adr[i] = 0;//green
+		if (i % 4 == 2)
+		data.get_adr[i] = -1;//red
+		if (i % 4 == 3)
+			data.get_adr[i] = 0;//more it become big the color becames less bright 
+		i++;
+	}
+	mlx_put_image_to_window(data.mlx, data.mlx_win, data.img, pos.x, pos.y);
+}
+
+void	render_map(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			if (map[i][j] == '')
+		}
+	}
+	
+}
+
+//char	*mlx_get_data_addr(void *img_ptr, int *bits_per_pixel, int *size_line, int *endian);
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	char	*map;
+	t_pos	pos;
+	t_data	data;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1000, 1000, "Cub3D");
-	
-	mlx_pixel_put(mlx, mlx_win, 500, 500, 0x00FF0000);
-	mlx_loop(mlx);
+	data.mlx = mlx_init();
+	data.mlx_win = mlx_new_window(data.mlx, 1000, 1000, "Cub3D");
+	data.map = get_map();
+	pos.x = 0;
+	pos.y = 0;
+	data.map = get_map();
+	render_map(data);
+	put_square(pos, 100, data);
+	//printf("bits_per_pixel:%d, size_line:%d, endian:%d\n", bits_per_pixel, size_line, endian);
+	mlx_loop(data.mlx);
 }
